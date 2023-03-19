916274a61fee0b38db49459df7ac8f2c86249a9b
lexalevi4 <lexalevi4@yandex.ru>
2023-03-19 20:04:29 +0300

    19.03

--- src/pages/FlatList.jsx
+++ src/pages/FlatList.jsx
@@ -1,37 +1,61 @@
-import {useGetFlatsQuery} from "../store/PyxiApi";
-import FlatCard from "../compontents/FlatCard";
-import {Container, Row} from "react-bootstrap";
-import {retry} from "@reduxjs/toolkit/query";
-import {useSelector} from "react-redux";
+import { useGetFlatsQuery } from "../store/PyxiApi";
+import FlatCardMu from "../compontents/FlatCardMu";
 
-const FlatList = function () {
+// import { retry } from "@reduxjs/toolkit/query";
+import { useDispatch, useSelector } from "react-redux";
+import {  Pagination, Paper } from "@mui/material";
+// import { useState } from "react";
+import { updateSearch } from "store/MapFlatsSlice";
 
+
+const FlatList = function ({handlePriceDescModal}) {
 
     const search = useSelector(state => state.mapFlats.search);
-    const {data, isError, isLoading} = useGetFlatsQuery(search)
-    // console.log(flats);
+    const { data, isLoading } = useGetFlatsQuery(search)
+
+    const dispatch = useDispatch();
+    const handlePage = (event, value) => {
+        dispatch(updateSearch({ field: 'page', value: value }))
+        window.scrollTo(0, 0)
+    }
+
+
+
+
+
+
+
 
     if (isLoading) {
 
         return (<h1>Loading</h1>)
     }
-    console.log(data);
 
     return (
-        <Container>
+        <>
+            <Paper
+                key={'flat_list'}
+            >
+                {
+                    data.flats.map(function (flat) {
 
-            {
+                        return (<FlatCardMu handlePriceDescModal={handlePriceDescModal} key={'flat_card_' + flat.id} flat={flat} />)
+                    })
+                }
+            </Paper>
+            <Pagination
+                className="mb-5 mt-3"
+                count={Math.ceil(data.total_count / 10)} page={search.page}
+                onChange={handlePage}
+            />
+            
 
-                data.map(function (flat) {
-
-                    return (<Row key={flat.id}> <FlatCard flat={flat}/></Row>)
-
-                })
-            }
-        </Container>
+        </>
 
     )
 
 }
+
+
 
 export default FlatList;
